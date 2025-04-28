48fe39a576c2720b3ece6e0146e1e7524eb8ecc1
farreltama <farrel2704@gmail.com>
2025-04-28 17:25:26 +0700

    update statistic page

--- lib/pages/statistic.dart
+++ lib/pages/statistic.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class StatisticPage extends StatelessWidget {
   const StatisticPage({super.key});
@@ -55,15 +56,21 @@ class _MonitoringSectionState extends State<MonitoringSection> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        // TODO: Navigasi ke halaman detail
         ScaffoldMessenger.of(
           context,
-        ).showSnackBar(SnackBar(content: Text("Klik  ${widget.title}")));
+        ).showSnackBar(SnackBar(content: Text("Klik ${widget.title}")));
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Kotak monitoring
+          // Title monitoring di atas
+          Text(
+            "Monitoring ${widget.title}",
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+
+          // Kotak monitoring berisi grafik dummy
           Container(
             height: 150,
             width: double.infinity,
@@ -71,13 +78,27 @@ class _MonitoringSectionState extends State<MonitoringSection> {
               color: const Color(0xFF184C45),
               borderRadius: BorderRadius.circular(16),
             ),
-            alignment: Alignment.center,
-            child: Text(
-              "Monitoring",
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+            padding: const EdgeInsets.all(12),
+            child: LineChart(
+              LineChartData(
+                gridData: FlGridData(show: false),
+                titlesData: FlTitlesData(show: false),
+                borderData: FlBorderData(show: false),
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: [
+                      FlSpot(0, 3),
+                      FlSpot(1, 1.5),
+                      FlSpot(2, 2),
+                      FlSpot(3, 1.8),
+                      FlSpot(4, 2.8),
+                    ],
+                    isCurved: true,
+                    color: Colors.white,
+                    dotData: FlDotData(show: false),
+                    belowBarData: BarAreaData(show: false),
+                  ),
+                ],
               ),
             ),
           ),
