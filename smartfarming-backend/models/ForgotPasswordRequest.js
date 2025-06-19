5c23ce27bcc760bb70717c3102a83c8276b96972
muhammadabyann <abyanrs46@gmail.com>
2025-06-19 11:46:17 +0700

    update forgotpassword

--- smartfarming-backend/models/ForgotPasswordRequest.js
+++ smartfarming-backend/models/ForgotPasswordRequest.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
-const mongoose = require('mongoose');
 
-const forgotPasswordRequestSchema = new mongoose.Schema({
-  userEmail: String,
-  createdAt: { type: Date, default: Date.now },
-  status: { type: String, enum: ['pending', 'handled'], default: 'pending' }
-});
+const forgotPasswordRequestSchema = new mongoose.Schema(
+  {
+    userEmail: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'handled'],
+      default: 'pending',
+    },
+  },
+  { timestamps: true } // ✅ INI YANG PENTING
+);
 
 module.exports = mongoose.model('ForgotPasswordRequest', forgotPasswordRequestSchema);
